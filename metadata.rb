3bcd0686acd9c8c5191069a07f411ed3fa6f78e9
appteamio <jalil@appteam.io>
2015-01-16 14:35:12 -0500

    removed metadata; was causing issues with vagrant

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'Crockpot'
-maintainer       'App Team'
-maintainer_email 'jalil@appteam.io'
-license          'All rights reserved'
-description      'selection of recipes to bootstrap a rails/php development '
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
